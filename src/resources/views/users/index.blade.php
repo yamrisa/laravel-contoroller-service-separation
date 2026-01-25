5a9b9fd9414f12ac053c04c74f62d4453f8ff349
yamrisa <riffiy.6135@gmail.com>
2026-01-25 10:46:38 +0900

    init: Laravel docker environment

---
+++ src/resources/views/users/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Users</title>
+</head>
+<body>
+    <h1>User一覧</h1>
+    <p>Laravelの画面が表示されました 🎉</p>
+</body>
+</html>
---
+++ src/routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::get('/users', [UserController::class, 'index']);
