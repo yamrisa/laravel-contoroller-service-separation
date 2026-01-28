a2d1ded7f2c4b1d464c1c2942be2a4d147e0e770
yamrisa <riffiy.6135@gmail.com>
2026-01-28 13:32:44 +0900

    add/domment

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+// use App\Models\User;
+// UserModelを使う責任をUserServiceへ移行
+use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function store(Request $request)
+    // public function store(Request $request)
+    public function store(Request $request, UserService $userService)
     {
         // バリデーション
         $request->validate([
@@ -16,21 +19,26 @@ class UserController extends Controller
             'age' => 'required|integer',
         ]);
 
-        // 年齢チェック
-        if ($request->age < 20) {
-            return back()->with('error', '20歳未満は登録できません');
+        // // 年齢チェック
+        // if ($request->age < 20) {
+        //     return back()->with('error', '20歳未満は登録できません');
+        // }
+
+        // // ユーザー作成
+        // $user = User::create([
+        //     'name' => $request->name,
+        //     'email' => $request->email,
+        //     'age' => $request->age,
+        //     'password' => bcrypt('password'),
+        // ]);
+
+        // 上記の処理はServiceに移行、今後も増えるならUserRegisterService作成してユースケースごとに
+        try {
+            $userService->create($request->all());
+        } catch (\Exception $e) {
+            return back()->with('error', $e->getMessage());
         }
 
-        // ユーザー作成
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'age' => $request->age,
-            'password' => bcrypt('password'),
-        ]);
-
-        // 初期設定（仮）
-        // ここに「初回ポイント付与」とか増えていく想定
 
         return redirect('/users')->with('success', '登録完了');
     }
